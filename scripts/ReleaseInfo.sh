43c156274d108c46824d4d08b1f8fe0c88da7bcb
GideonWhite1029 <gideonwhite1029@gmail.com>
2024-04-22 17:15:13 +0400

    Test Workflow 8

--- scripts/ReleaseInfo.sh
+++ scripts/ReleaseInfo.sh
@@ -1,51 +1,29 @@
-sha1() {
-  sha1sum $1 | awk '{print $1}'
-}
-
-md5() {
-  md5sum $1 | awk '{print $1}'
-}
-
 prop() {
-  grep "${1}" gradle.properties | cut -d'=' -f2 | sed 's/\r//'
+  grep "^[[:space:]]*${1}" gradle.properties | cut -d'=' -f2 | sed 's/^[[:space:]]*//; s/\r//'
 }
+
+project_id="purpurasync"
+project_id_b="PurpurAsync"
 
 commitid=$(git log --pretty='%h' -1)
 mcversion=$(prop mcVersion)
-gradleVersion=$(prop version)
+grdversion=$(prop version)
 preVersion=$(prop preVersion)
-tagid="$mcversion-$commitid"
-jarName="purpurasync-$mcversion.jar"
-purpurasyncid="PurpurAsync-$commitid"
-releaseinfo="releaseinfo.md"
+release_tag="$mcversion-$commitid"
+jarName="$project_id-$mcversion-paperclip.jar"
+jarName_dir="build/libs/$jarName"
 make_latest=$([ $preVersion = "true" ] && echo "false" || echo "true")
 
-rm -f $releaseinfo
+mv build/libs/$project_id_b-paperclip-$grdversion-reobf.jar build/libs/$jarName
+mv build/libs/$project_id_b-bundler-$grdversion-reobf.jar build/libs/$project_id-$mcversion-bundler.jar
 
-mv build/libs/PurpurAsync-paperclip-$gradleVersion-reobf.jar $jarName
-echo "name=$purpurasyncid" >> $GITHUB_ENV
-echo "tag=$tagid" >> $GITHUB_ENV
-echo "jar=$jarName" >> $GITHUB_ENV
-echo "info=$releaseinfo" >> $GITHUB_ENV
+echo "project_id=$project_id" >> $GITHUB_ENV
+echo "project_id_b=$project_id_b" >> $GITHUB_ENV
+echo "commit_id=$commitid" >> $GITHUB_ENV
+echo "commit_msg=$(git log --pretty='> [%h] %s' -1)" >> $GITHUB_ENV
+echo "mcversion=$mcversion" >> $GITHUB_ENV
 echo "pre=$preVersion" >> $GITHUB_ENV
+echo "tag=$release_tag" >> $GITHUB_ENV
+echo "jar=$jarName" >> $GITHUB_ENV
+echo "jar_dir=$jarName_dir" >> $GITHUB_ENV
 echo "make_latest=$make_latest" >> $GITHUB_ENV
-
-echo "$purpurasyncid [![download](https://img.shields.io/github/downloads/GideonWhite1029/PurpurAsync/$tagid/total?color=0)](https://github.com/GideonWhite1029/PurpurAsync/releases/download/$tagid/$jarName)" >> $releaseinfo
-echo "=====" >> $releaseinfo
-echo "" >> $releaseinfo
-if [ $preVersion = "true" ]; then
-  echo "> This is an early, experimental build. It is only recommended for usage on test servers and should be used with caution." >> $releaseinfo
-  echo "> **Backups are mandatory!**" >> $releaseinfo
-  echo "" >> $releaseinfo
-fi
-echo "### Commit Message" >> $releaseinfo
-
-number=$(git log --oneline master ^`git describe --tags --abbrev=0` | wc -l)
-echo "$(git log --pretty='> [%h] %s' -$number)" >> $releaseinfo
-
-echo "" >> $releaseinfo
-echo "### Checksum" >> $releaseinfo
-echo "| File | $jarName |" >> $releaseinfo
-echo "| ---- | ---- |" >> $releaseinfo
-echo "| MD5 | `md5 $jarName` |" >> $releaseinfo
-echo "| SHA1 | `sha1 $jarName` |" >> $releaseinfo
